08ce19e91dd1815aacea841635cd5204c0c05f4a
Alex Luna <alexlunapuerto@gmail.com>
2023-12-03 18:04:13 +0100

    Day 2 part 2 solved

--- advent-of-code/2023/day-2/part-2/part-2.py
+++ advent-of-code/2023/day-2/part-2/part-2.py
@@ -1,46 +1,35 @@
 file = open('input.txt', 'r')
 text = file.readlines()
 
-game_id_sum = 0
-
-max_red = 12
-max_green = 13
-max_blue = 14
+total_power = 0
 
 for line in text:
     splitted_game = line.split(":")
     game = splitted_game[0].split(" ")
     sets = splitted_game[1].split(";")
 
-    game_id = int(game[1])
+    red_max = 1
+    green_max = 1
+    blue_max = 1
 
-    # Game is valid by default
-    valid_game = True
-    
+    set_power = 1
+
     for set in sets:
-        red_count = 0
-        green_count = 0
-        blue_count = 0
-
         subsets = set.split(",")
 
         for subset in subsets:
             splitted_subsets = subset.split(" ")
-            cube_quantity = splitted_subsets[1]
+            cube_quantity = int(splitted_subsets[1])
             cube_color = splitted_subsets[2].strip("\n")
 
-            if cube_color == "red": 
-                red_count += int(cube_quantity)
-            if cube_color == "green": 
-                green_count += int(cube_quantity)
-            if cube_color == "blue": 
-                blue_count += int(cube_quantity)
+            if cube_color == "red" and cube_quantity > red_max: 
+                red_max = cube_quantity
+            if cube_color == "green" and cube_quantity > green_max: 
+                green_max = cube_quantity
+            if cube_color == "blue" and cube_quantity > blue_max: 
+                blue_max = cube_quantity
         
-        if (red_count > max_red) or (green_count > max_green) or (blue_count > max_blue):
-            # If a color quantity exceeds max value game is no longer valid
-            valid_game = False
-            break
-    
-    if valid_game == True: game_id_sum += game_id
+    set_power = red_max * blue_max * green_max
+    total_power += set_power
 
-print(game_id_sum)
+print (total_power)
